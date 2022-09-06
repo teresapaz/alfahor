b1a84845cad114ff465a1feb61c9f600f7010075
teresapaz <teresapaz.paneque@gmail.com>
2022-09-06 10:47:25 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="alfahor",
+    version="1.0",
+    author="Teresa Paneque-Carreño",
+    author_email='tpaneque@eso.org',
+    packages=["alfahor"],
+    url="https://github.com/teresapaz/alfahor",
+    license="LICENSE.md",
+    description=("Vertical structure from masked channel maps."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy",
+        "astropy"]
+    }
+)
